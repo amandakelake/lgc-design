b60a8032f6a39daf371167eebb658aac48df59ac
皛白 <930007017@qq.com>
2019-04-03 17:28:05 +0800

    init router

--- examples/main.js
+++ examples/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import router from "./router/router"
 import App from './App.vue'
+import LgcDesign from "../packages"
 
 Vue.config.productionTip = false
 
+Vue.use(LgcDesign)
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ examples/pages/home/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="btn-container">
+      <lgc-button color="#333" bgColor="red">button1</lgc-button>
+      <lgc-button-2 title="button2" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home"
+}
+</script>
+
+<style scoped>
+.btn-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
